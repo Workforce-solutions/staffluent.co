4cf5b423ac82134f89f8aae50d325d319fe7030c
Griseld <development@venueboost.io>
2025-02-21 00:41:12 +0200

    fix build error

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const config = {
-    output: 'export',
     images: {
         unoptimized: true,
     },
     assetPrefix: '/',
-    trailingSlash: true,  // Add this
-    basePath: '',         // Add this
 };
 
 export default config;
