ca32968490e6c63c23584306acacc7b4f8ee9562
Griseld <development@venueboost.io>
2025-02-25 15:36:39 +0200

    fix lint

--- src/app/subscription/page.tsx
+++ src/app/subscription/page.tsx
@@ -2,6 +2,7 @@
 import { Metadata } from 'next';
 import styles from "../page.module.css";
 import SubscriptionPageWrapper from "@/pages/SubscriptionPageWrapper";
+import { Suspense } from 'react';
 import { METADATA } from '@/constants/metadata';
 
 export const metadata: Metadata = {
@@ -12,7 +13,19 @@ export const metadata: Metadata = {
 export default function Subscription() {
   return (
     <div className={styles.homeDesktop}>
-      <SubscriptionPageWrapper />
+     <Suspense fallback={<SubscriptionLoading />}>
+        <SubscriptionPageWrapper />
+      </Suspense>
+    </div>
+  );
+}
+
+
+// Loading component to show while the page is loading
+function SubscriptionLoading() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#0A0A0A]"></div>
     </div>
   );
 }
