f38fb716f87912b5106295934cd9e14b21c44354
Griseld <development@venueboost.io>
2025-02-25 17:54:32 +0200

    fix: build

--- src/app/subscription-success/page.tsx
+++ src/app/subscription-success/page.tsx
@@ -3,6 +3,7 @@ import { Metadata } from 'next';
 import styles from "../page.module.css";
 import SubscriptionSuccessPageWrapper from "@/pages/SubscriptionSuccessPageWrapper";
 import { METADATA } from '@/constants/metadata';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
     title: METADATA.subscriptionSuccess.title,
@@ -12,7 +13,18 @@ export const metadata: Metadata = {
 export default function SubscriptionSuccess() {
   return (
     <div className={styles.homeDesktop}>
-      <SubscriptionSuccessPageWrapper />
+     <Suspense fallback={<SubscriptionSuccessLoading />}>
+        <SubscriptionSuccessPageWrapper />
+      </Suspense>
+    </div>
+  );
+}
+
+// Loading component to show while the page is loading
+function SubscriptionSuccessLoading() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#0A0A0A]"></div>
     </div>
   );
 }
